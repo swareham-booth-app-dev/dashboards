773faff894ad29e2f20cc52d1cb838f57878e3d3
swareham-booth <swareham@chicagobooth.edu>
2021-04-28 04:17:36 +0000

    100%

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -8,4 +8,23 @@ class CurrenciesController < ApplicationController
     render({:template => "currency_templates/step_one.html.erb"})
   end
 
+  def second_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = symbols_hash.keys
+    @from_currency = params.fetch("from_currency")
+    render({:template => "currency_templates/step_two.html.erb"})
+  end
+
+  def third_currency
+    @from_currency = params.fetch("from_currency")
+    @to_currency = params.fetch("to_currency")
+    url = "https://api.exchangerate.host/convert?from=" + @from_currency + "&to=" + @to_currency
+    @raw_data = open(url).read
+    @parsed_data = JSON.parse(@raw_data)
+    @exchange_rate = @parsed_data.fetch("result")
+    render({:template => "currency_templates/step_three.html.erb"})
+  end
+
 end
